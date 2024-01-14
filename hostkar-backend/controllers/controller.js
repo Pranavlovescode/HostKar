048ec2b1d5e2397b0e7f72d1c4a55df028820cde
Pranavlovescode <pranavtitambe04@gmail.com>
2024-01-14 15:45:12 +0530

    Using the validation

--- hostkar-backend/controllers/controller.js
+++ hostkar-backend/controllers/controller.js
@@ -1,12 +1,30 @@
 const Login = require("../models/signup");
-const bcrypt = require("bcrypt");
-const handleSignup = async (req, res) => {
-  const { name, email, pass } = req.body;
-  const hashPassword = await bcrypt.hash(pass, 10);
-  const result = await Login.create({ name, email, pass: hashPassword });
 
-  res.status(200).send(result);
-  console.log(result);
+const handelError = (err) => {
+  console.log(err.message, err.code);
+  let error = { name: "", email: "", pass: "" };
+  if (err.message.includes("login validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      error[properties.path] = properties.message;
+    });
+  }
+  if (err.code === 11000) {
+    error.email = "Email already registered";
+    return error;
+  }
+  return error;
+};
+const handleSignup = async (req, res) => {
+  try {
+    const { name, email, pass } = req.body;
+    const result = await Login.create({ name, email, pass });
+
+    res.status(200).send(result);
+    console.log(result);
+  } catch (error) {
+    const errors = handelError(error);
+    res.status(400).json({ errors });
+  }
 };
 
 module.exports = { handleSignup };
--- hostkar-backend/models/signup.js
+++ hostkar-backend/models/signup.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose
-const loginSchema = new Schema({
-  name: { type: String ,required:true},
-  email: { type: String, required: true },
-  pass: { type: String, required: true },
-},{timestamps:true});
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
+const { Schema } = mongoose;
+const loginSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter a email"],
+      unique: true,
+      validate: [isEmail, "Please enter a valid email"],
+    },
+    pass: {
+      type: String,
+      required: [true, "Enter a password"],
+      minlength: [8, "Password should be at least 8 characters"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('login',loginSchema)
+loginSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.pass = await bcrypt.hash(this.pass, salt);
+  next();
+});
+module.exports = mongoose.model("login", loginSchema);
